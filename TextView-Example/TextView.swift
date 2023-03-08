2b20e321197c2cce0a95bfa58ec6c02923814bc1
Joseph Levy <josephlevy222@gmail.com>
2023-03-08 14:30:20 -0800

    Now working well except for the underline bug in iOS 15 with edit menus

--- TextView-Example/TextView.swift
+++ TextView-Example/TextView.swift
@@ -3,14 +3,14 @@
 //  TextView-Example
 //
 //  Created by Joseph Levy on 1/31/23.
-//
+//  Working for iOS 15 and 16 3/8/23
 import SwiftUI
 
 struct TextView: UIViewRepresentable {
     
     @Binding var attributedText: AttributedString
     @State var allowsEditingTextAttributes: Bool = false
-
+    
     let defaultFont = UIFont.preferredFont(from: .body)
     
     
@@ -20,11 +20,6 @@ struct TextView: UIViewRepresentable {
         uiView.typingAttributes = [.font : defaultFont ]
         uiView.allowsEditingTextAttributes = allowsEditingTextAttributes
         uiView.textContainerInset = .zero
-        if #available(iOS 16.0, *) {
-            //uiView.editMenu(for: .init(), suggestedActions: [] )
-        } else {
-            // Fallback on earlier versions
-        } //iOS 16 only
         uiView.contentInset = UIEdgeInsets()
         uiView.delegate = context.coordinator
         uiView.attributedText = attributedText.nsAttributedString
@@ -46,34 +41,22 @@ struct TextView: UIViewRepresentable {
             self.text = text
         }
         
-        func editMenu(for textRange: UITextRange, suggestedActions: [UIMenuElement]) -> UIMenu? {
-            let indentationMenu = UIMenu(title: "Indentation", image: UIImage(systemName: "list.bullet.indent"), children: [
-                UIAction(title: "Increase", image: UIImage(systemName: "increase.indent")) { (action) in
-                    // Increase indentation action.
-                },
-                UIAction(title: "Decrease", image: UIImage(systemName: "decrease.indent")) { (action) in
-                    // Decrease indentation action.
-                }
-            ])
-            
-            var actions = suggestedActions
-            actions.append(indentationMenu)
-            return UIMenu(children: actions)
-        }
         func textViewDidChange(_ textView: UITextView) {
             let oldValue = textView.attributedText ?? NSAttributedString()
             let newValue = NSMutableAttributedString(attributedString: oldValue)
-            print("update",newValue)
+            //print("update",newValue)
             let newAS = { do { return try AttributedString(oldValue, including: \.uiKit) }
                 catch { return AttributedString(oldValue)}}().resetFonts()
             self.text.wrappedValue = {
                 var aString = AttributedString()
-                newValue.enumerateAttributes(in: NSRange(location: 0, length: newValue.length)) { (attributes, range, stopFlag) in
+                newValue.enumerateAttributes(in: NSRange(location: 0,
+                                                         length: newValue.length)) { (attributes, range, stopFlag) in
                     var newRun = AttributedString()
                     if let indexRange = Range(range, in: newAS) {
                         newRun = AttributedString(newAS[indexRange])
                         if let strikethroughStyle = attributes[.strikethroughStyle] {
-                            newRun.strikethroughStyle = strikethroughStyle as? Text.LineStyle ?? .init(pattern: .solid, color: nil)
+                            newRun.strikethroughStyle =
+                            strikethroughStyle as? Text.LineStyle ?? .init(pattern: .solid, color: nil)
                         }
                     }
                     aString.append(newRun)
@@ -83,20 +66,42 @@ struct TextView: UIViewRepresentable {
             textView.attributedText = text.wrappedValue.nsAttributedString
         }
     }
-
+    
     class MyTextView: UITextView {
+        // This works in iOS 16 never called in 15 I believe
+        open override func buildMenu(with builder: UIMenuBuilder) {
+            builder.remove(menu: .lookup) // Lookup, Translate, Search Web
+            //builder.remove(menu: .standardEdit) // Keep Cut, Copy, Paste
+            //builder.remove(menu: .replace) // Keep Replace
+            builder.remove(menu: .share) // Remove Share
+            //builder.remove(menu: .textStyle) // Keep Format
+            // Add new .textStyle actions
+            let strikethroughAction = UIAction(title: "Strikethough") { action in
+                self.toggleStrikethrough(action.sender)
+            }
+            builder.replaceChildren(ofMenu: .textStyle)  {
+                var children = $0
+                children.append(strikethroughAction)
+                return children
+            }
+            super.buildMenu(with: builder)
+        }
         
-        override func canPerformAction(_ action: Selector, withSender sender: Any?) -> Bool {
-            let menuController = UIMenuController.shared
-            if var menuItems = menuController.menuItems,
-               (menuItems.map { $0.title })
-                .elementsEqual(["Bold", "Italic", "Underline"]) {
-                // The font style menu is about to become visible -- Add a new menu item for strikethrough style
-                // iOS 16 must use UIEditMenuIteraction instead (How?) since UIMenuItem is deprecated in iOS 16
-                //let editMenuInteraction = UIEditMenuInteraction(delegate: UIMenu) //UIEditMenuInteractionDelegate())
-                menuItems.append(UIMenuItem(title: "Strikethrough", action: .toggleStrikethrough))
-                menuItems.append(UIMenuItem(title: "More", action: .moreItems))
-                menuController.menuItems = menuItems
+        // This is neetded for iOS 15
+        open override func canPerformAction(_ action: Selector, withSender sender: Any?) -> Bool {
+            if #unavailable(iOS 16.0) {
+                let menuController = UIMenuController.shared
+                if var menuItems = menuController.menuItems,
+                   menuItems[0].title == "Bold" && menuItems.count < 4 {
+                    menuItems.append(UIMenuItem(title: "Strikethrough", action: .toggleStrikethrough))
+                    menuController.menuItems = menuItems
+                }
+                // Get rid of menu item not wanted
+                if action.description.contains("_share") // Share
+                    || action.description.contains("_translate") // Translate
+                    || action.description.contains("_define") { // Blocks Lookup
+                    return false
+                }
             }
             return super.canPerformAction(action, withSender: sender)
         }
@@ -104,7 +109,9 @@ struct TextView: UIViewRepresentable {
         @objc func toggleStrikethrough(_ sender: Any?) {
             let attributedString = NSMutableAttributedString(attributedString: attributedText)
             var isAllStrikethrough = true
-            attributedString.enumerateAttribute(.strikethroughStyle, in: selectedRange, options: []) {(value, range, stopFlag) in
+            attributedString.enumerateAttribute(.strikethroughStyle,
+                                                in: selectedRange,
+                                                options: []) { (value, range, stopFlag) in
                 let strikethrough = value as? NSNumber
                 if strikethrough == nil {
                     isAllStrikethrough = false
@@ -123,18 +130,23 @@ struct TextView: UIViewRepresentable {
         @objc override func toggleUnderline(_ sender: Any?) {
             let attributedString = NSMutableAttributedString(attributedString: attributedText)
             var isAllUnderlined = true
-            attributedString.enumerateAttribute(.underlineStyle, in: selectedRange, options: []) {(value, range, stopFlag) in
+            attributedString.enumerateAttribute(.underlineStyle,
+                                                in: selectedRange,
+                                                options: []) { (value, range, stopFlag) in
                 let underline = value as? NSNumber ?? 0
                 if underline == 0  {
                     isAllUnderlined = false
                     stopFlag.pointee = true
                 }
             }
-            attributedString.enumerateAttributes(in: selectedRange, options: []) {(value, range, stopFlag) in
+            attributedString.enumerateAttributes(in: selectedRange,
+                                                 options: []) {(value, range, stopFlag) in
                 if isAllUnderlined {
                     attributedString.removeAttribute(.underlineStyle, range: range)
                 } else {
-                    attributedString.addAttribute(.underlineStyle, value: NSUnderlineStyle.single.rawValue, range: range)
+                    attributedString.addAttribute(.underlineStyle,
+                                                  value: NSUnderlineStyle.single.rawValue,
+                                                  range: range)
                 }
             }
             attributedText = attributedString
@@ -148,11 +160,15 @@ struct TextView: UIViewRepresentable {
         @objc override func toggleItalics(_ sender: Any?) {
             toggleSymbolicTrait(sender, trait: .traitItalic)
         }
-        
+        @objc  func interactions(_ sender: Any?)  {
+            print(interactions )
+        }
         private func toggleSymbolicTrait(_ sender: Any?, trait: UIFontDescriptor.SymbolicTraits) {
             let attributedString = NSMutableAttributedString(attributedString: attributedText)
             var isAll = true
-            attributedString.enumerateAttribute(.font, in: selectedRange, options: []) {(value, range, stopFlag) in
+            attributedString.enumerateAttribute(.font,
+                                                in: selectedRange,
+                                                options: []) { (value, range, stopFlag) in
                 let uiFont = value as? UIFont
                 if let descriptor = uiFont?.fontDescriptor {
                     let isTrait = descriptor.symbolicTraits.intersection(trait) == trait
@@ -160,13 +176,21 @@ struct TextView: UIViewRepresentable {
                     if !isTrait { stopFlag.pointee = true }
                 }
             }
-            attributedString.enumerateAttribute(.font, in: selectedRange, options: []) {(value, range, stopFlag) in
+            attributedString.enumerateAttribute(.font,
+                                                in: selectedRange,
+                                                options: []) {(value, range, stopFlag) in
                 let uiFont = value as? UIFont
                 if  let descriptor = uiFont?.fontDescriptor {
-                    if let fontDescriptor = isAll ? descriptor.withSymbolicTraits(descriptor.symbolicTraits.subtracting(trait))
-                        : descriptor.withSymbolicTraits(descriptor.symbolicTraits.union(trait))?.withWeight(trait == .traitBold ? .bold : nil ) {
-                                              //       Needed to fix Title0 bug in UITextView  ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
-                        attributedString.addAttribute(.font, value: UIFont(descriptor: fontDescriptor, size: descriptor.pointSize), range: range)
+                    if let fontDescriptor = isAll ?
+                        descriptor.withSymbolicTraits(descriptor.symbolicTraits.subtracting(trait))
+                        : descriptor
+                        .withSymbolicTraits(descriptor.symbolicTraits.union(trait))?
+                        .withWeight(trait == .traitBold ? .bold : nil ) {
+                        // ^ Needed to fix Title0 bug in UITextView ^
+                        attributedString
+                            .addAttribute(.font,
+                                          value: UIFont(descriptor: fontDescriptor, size: descriptor.pointSize),
+                                          range: range)
                     }
                 }
             }
@@ -189,8 +213,8 @@ fileprivate extension Selector {
     static let toggleItalics = #selector(TextView.MyTextView.toggleItalics(_:))
     static let toggleUnderline = #selector(TextView.MyTextView.toggleUnderline(_:))
     static let toggleStrikethrough = #selector(TextView.MyTextView.toggleStrikethrough(_:))
-    static let moreItems = #selector(TextView.MyTextView.moreItems(_:))
-    //static let toggleSubscript = #selector(TextView.MyTextView.toggleSubscript(_:))
+    static let toggleSubscript = #selector(TextView.MyTextView.toggleSubscript(_:))
 }
+
 
 
